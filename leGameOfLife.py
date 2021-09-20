f56b1d248fe2d8b2653ba2a9604d3d28be5f1727
Lefti <vlefti@hotmail.com>
2021-09-20 19:15:20 +0300

    Update leGameOfLife.py

--- leGameOfLife.py
+++ leGameOfLife.py
@@ -20,14 +20,19 @@
 # called a tick. Each generation is a pure function of the preceding one. The rules continue
 # to be applied repeatedly to create further generations.
 
-# TODO changeCell
 
 import pygame
 
-FPS = 5
+# Grid Size
+xSize = 70
+ySize = 40
+
+pausedFPS = 60
+unpausedFPS = 4
 
 BLACK = (0, 0, 0)
 GRAY = (127, 127, 127)
+GRAY2 = (90, 90, 90)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -39,8 +44,7 @@ MAGENTA = (255, 0, 255)
 pygame.display.set_caption('leGameOfLife')
 clock = pygame.time.Clock()
 
-xSize = 20
-ySize = 20
+
 
 dispMult = 20
 xDisplay = xSize * dispMult
@@ -57,7 +61,6 @@ map = []
 
 def initMap():
     map.clear()
-
     for y in range(ySize):
         tmp = []
         for x in range(xSize):
@@ -71,50 +74,54 @@ def drawMap():
             if   map[y][x] == LIVE:
                 pygame.draw.rect(gameDisplay, YELLOW, [x * dispMult , y * dispMult, dispMult, dispMult])
             elif map[y][x] == DEAD:
-                pygame.draw.rect(gameDisplay, BLACK, [x * dispMult , y * dispMult, dispMult, dispMult])
+                pygame.draw.rect(gameDisplay, GRAY2, [x * dispMult , y * dispMult, dispMult, dispMult])
             pygame.draw.rect(gameDisplay, GRAY, [x * dispMult , y * dispMult, dispMult, dispMult], 1)  
 
 
 def getNeighbours(y, x):
+    
     neighbours = 0
 
-    if (map[y-1][x] == LIVE)    and (y != 0): # TOP
+    if(y != 0)         and                      (map[y-1][x] == LIVE): # TOP
         neighbours += 1
-    if (map[y-1][x+1] == LIVE)  and (y != 0)         and (x != (xSize-1)): # TOP RIGHT
+    if(y != 0)         and (x != (xSize-1)) and (map[y-1][x+1] == LIVE): # TOP RIGHT
         neighbours += 1
-    if (map[y][x+1] == LIVE)    and (x != (xSize-1)): # RIGHT
+    if(x != (xSize-1)) and                      (map[y][x+1] == LIVE): # RIGHT
         neighbours += 1
-    if (map[y+1][x+1] == LIVE)  and (y != (ySize-1)) and (x != (xSize-1)): # BOTTOM RIGHT
+    if(y != (ySize-1)) and (x != (xSize-1)) and (map[y+1][x+1] == LIVE): # BOTTOM RIGHT
         neighbours += 1
-    if (map[y+1][x] == LIVE)    and (y != (ySize-1)): # BOTTOM
-        neighbours += 1
-    if (map[y+1][x-1] == LIVE)  and (y != (ySize-1)) and (x != 0): # BOTTOM LEFT
-        neighbours += 1
-    if (map[y][x-1] == LIVE)    and (x != 0): # LEFT
-        neighbours += 1
-    if (map[y-1][x-1] == LIVE)  and (y != 0)         and (x != 0): # TOP LEFT
-        neighbours += 1
+    if(y != (ySize-1)) and                      (map[y+1][x] == LIVE): # BOTTOM
+        neighbours += 1     
+    if(y != (ySize-1)) and (x != 0)         and (map[y+1][x-1] == LIVE): # BOTTOM LEFT
+        neighbours += 1     
+    if(x != 0)         and                      (map[y][x-1] == LIVE): # LEFT
+        neighbours += 1             
+    if(y != 0)         and (x != 0)         and (map[y-1][x-1] == LIVE): # TOP LEFT
+        neighbours += 1     
 
     return neighbours
 
 
 def gameUpdate():
+    neighboursMap = []
+
+    for y in range(ySize):
+        neighboursMap.append([])
+        for x in range(xSize):
+            neighboursMap[y].append(getNeighbours(y, x))
+    
     for y in range(ySize):
         for x in range(xSize):
-            neighbours = getNeighbours(y, x)
-            if   map[y][x] == LIVE:
-                if (neighbours != 2) and (neighbours != 3):
-                    map[y][x] = DEAD
-            elif map[y][x] == DEAD:
-                if neighbours == 3:
-                    map[y][x] = LIVE
+            if   (map[y][x] == LIVE) and (neighboursMap[y][x] != 2) and (neighboursMap[y][x] != 3):
+                map[y][x] = DEAD
+            elif (map[y][x] == DEAD) and (neighboursMap[y][x] == 3):
+                map[y][x] = LIVE
 
 
-# TODO
 def changeCell(mouseY, mouseX):
-    curY = mouseY
-    curX = mouseX
-
+    curY = mouseY // dispMult
+    curX = mouseX // dispMult
+    map[curY][curX] = not map[curY][curX]
 
 
 def main(): 
@@ -124,40 +131,54 @@ def main():
 
     PAUSE = True
 
+    font = pygame.font.SysFont(None, 25)
+    text = font.render('Left Click: Flip Cell , R: Reset Grid , N: Next State , SPACE: Un/Pause , PAUSED', True, WHITE)
 
     while True:
-        keyPress = False
-        leftMousePress = False
+        gameDisplay.fill(BLACK)
+        drawMap()
+        gameDisplay.blit(text, (10 , yDisplay + dispMult/2))
+
+        buttonPress = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
             if event.type == pygame.KEYDOWN:
-                if keyPress == False:
+                if buttonPress == False:
                     if (event.key == pygame.K_SPACE):
                         PAUSE = not PAUSE
-                        keyPress = True
+                        buttonPress = True
+                        if PAUSE == True:
+                            text = font.render('Left Click: Flip Cell , R: Reset Grid , N: Next State , SPACE: Un/Pause , PAUSED', True, WHITE)
+                        else:
+                            text = font.render('Left Click: Flip Cell , R: Reset Grid , N: Next State , SPACE: Un/Pause , UNPAUSED', True, WHITE)
                     elif (event.key == pygame.K_r):
                         initMap()
                         PAUSE = True
-                        keyPress = True
+                        buttonPress = True
+                    elif (event.key == pygame.K_n) and (PAUSE == True):
+                        gameUpdate()
+                        buttonPress = True
             if event.type == pygame.MOUSEBUTTONUP:
-                if leftMousePress == False:
+                if buttonPress == False:
                     if (event.button == 1):
-                        leftMousePress = True
+                        buttonPress = True
                         mouseX, mouseY = pygame.mouse.get_pos()
-                        changeCell(mouseY, mouseX)
-
-        gameDisplay.fill(BLACK)
-        drawMap()
+                        if (mouseY // dispMult) < ySize:
+                            changeCell(mouseY, mouseX)
 
         if PAUSE == False:
             gameUpdate()
 
         pygame.display.update()
-        clock.tick(FPS)
 
+        if PAUSE == True:
+            clock.tick(pausedFPS)
+        else:
+            gameUpdate()
+            clock.tick(unpausedFPS)
 
 if __name__ == "__main__":
     main()
